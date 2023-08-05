6dec4d86349e8680b58ef8ce90a1dcb7a6b3bdfc
AnkanHalder <halderankan95@gmail.com>
2023-08-05 09:18:58 +0530

    add Loading Page

--- src/page-components/homePage.jsx
+++ src/page-components/homePage.jsx
@@ -28,11 +28,10 @@ const HomePage = () => {
       <>  
           <h1 className='text-2xl m-8'>Searching For: {searchValue }</h1>
           <div className='flex flex-wrap items-center justify-center'>
-          {
-            bList.filter(item => {
-            const {name,author,genre,...rest} = item;
-            return [author,name,genre].some(value => (new RegExp(`.*${searchValue}.*`, "i")).test(value))}).map((b)=> <Card details={b} />)
-          }
+          {bList.filter(item => {
+            const { name, author, genre, ...rest } = item;
+            return [author, name, genre].some(value => (new RegExp(`.*${searchValue}.*`, "i")).test(value));
+          }).map((b) => <Card key={b._id} details={b} />)}
           </div>
       </>}
       
