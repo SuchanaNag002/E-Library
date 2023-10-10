937ab362121f0197c6e409b47af343ec5b875d25
Suchana Nag <99524477+SuchanaNag002@users.noreply.github.com>
2023-10-10 16:40:56 +0530

    Update authContext.js
    minor changes

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,41 +1,57 @@
-"use client"
-import { useContext,createContext,useState,useEffect } from "react";
-import { GoogleAuthProvider,signOut,signInWithPopup,onAuthStateChanged } from "firebase/auth";
+"use client";
+import { useContext, createContext, useState, useEffect } from "react";
+import {
+  GoogleAuthProvider,
+  signOut,
+  signInWithPopup,
+  onAuthStateChanged,
+} from "firebase/auth";
 import { auth } from "@/firebase/firebaseConfig";
 import apiCaller from "@/api/apiCaller";
 
 const AuthContext = createContext();
 
-export const AuthContextProvider =({children}) =>{
-    const [user,setUser] = useState(null);
-    const [userDetails,setUserDetails] = useState(null);
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [userDetails, setUserDetails] = useState(null);
 
-    const logIn = ()=>{
-        const googleAuth = new GoogleAuthProvider();
-        signInWithPopup(auth,googleAuth);
-    };
-    const logOut = () => {
-        signOut(auth);
-    }
-    const setUserSettings = (currentUser) =>{
-        if(currentUser){
-            apiCaller.getUserDetails(currentUser.email, currentUser.displayName).then((data)=>{
-                setUserDetails(data);
-            });
-            
-        }
-    }
-
-    useEffect(()=>{
-        const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
-            setUser(currentUser);
-            setUserSettings(currentUser);
+  const logIn = () => {
+    const googleAuth = new GoogleAuthProvider();
+    signInWithPopup(auth, googleAuth);
+  };
+  const logOut = () => {
+    signOut(auth);
+  };
+  const setUserSettings = (currentUser) => {
+    if (currentUser) {
+      apiCaller
+        .getUserDetails(currentUser.email, currentUser.displayName)
+        .then((data) => {
+          setUserDetails(data);
         });
-        return ()=> unsubscribe
-    },[user]);
-    return <AuthContext.Provider value={{user,userDetails,logIn,logOut,setUserDetails}}>{children}</AuthContext.Provider>
-}
+    }
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser); // Use the user state managed in this context
+      if (currentUser) {
+        setUserSettings(currentUser);
+      } else {
+        setUserSettings(null); // Set userDetails to null when user is not authenticated
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+  return (
+    <AuthContext.Provider
+      value={{ user, userDetails, logIn, logOut, setUserDetails }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export const UserAuth = () => {
-    return useContext(AuthContext);
-}
+  return useContext(AuthContext);
+};
