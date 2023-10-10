362c6525edf2aae97161cf492ea4e23e4d418505
Suchana Nag <99524477+SuchanaNag002@users.noreply.github.com>
2023-10-10 16:29:33 +0530

    Update apiCaller.jsx
    changed the date function

--- src/api/apiCaller.jsx
+++ src/api/apiCaller.jsx
@@ -1,34 +1,39 @@
 import requests from "./requests";
 import serverInstance from "./axiosInstance";
 
-class apiCaller{
-    static async getUserDetails(email,name){
-        const data = await serverInstance.get(requests.getUserDetails + email+"/"+name);
-        return data.data;
-    }
-    static async searchByCatagory(cat){
-        const data = await serverInstance.get(requests.searchByCatagory + cat);
-        return data.data;
-    }
-    static async search(limit){
-        const data = await serverInstance.get(requests.search + limit);
-        return data.data;
-    }
-    static async searchByID(id){
-        const data = await serverInstance.get(requests.searchByID + id);
-        return data.data;
-    }
-    static async addToCart(email,id){
-        const data = await serverInstance.get(requests.addToCart + email+"/"+id);
-        return data.data;
-    }
-    static async addToMyBooks(email,id){
-        let currentTime = new Date().getTime();
-        console.log(requests.addToMyBooks + email+"/"+id + "/" + currentTime)
-        const data = await serverInstance.get(requests.addToMyBooks + email+"/"+id + "/" + currentTime);
-        return data.data;
-    }
-
+class apiCaller {
+  static async getUserDetails(email, name) {
+    const data = await serverInstance.get(
+      requests.getUserDetails + email + "/" + name
+    );
+    return data.data;
+  }
+  static async searchByCatagory(cat) {
+    const data = await serverInstance.get(requests.searchByCatagory + cat);
+    return data.data;
+  }
+  static async search(limit) {
+    const data = await serverInstance.get(requests.search + limit);
+    return data.data;
+  }
+  static async searchByID(id) {
+    const data = await serverInstance.get(requests.searchByID + id);
+    return data.data;
+  }
+  static async addToCart(email, id) {
+    const data = await serverInstance.get(
+      requests.addToCart + email + "/" + id
+    );
+    return data.data;
+  }
+  static async addToMyBooks(email, id) {
+    let currentTime = new Date().toString();
+    //console.log(requests.addToMyBooks + email+"/"+id + "/" + currentTime)
+    const data = await serverInstance.get(
+      requests.addToMyBooks + email + "/" + id + "/" + currentTime
+    );
+    return data.data;
+  }
 }
 
 export default apiCaller;
